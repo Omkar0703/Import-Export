bc247d8deaa5810209b108ee34fa29d4b29c97bd
Omkar0703 <107262648+Omkar0703@users.noreply.github.com>
2022-09-04 07:09:41 -0700

    Update navbar.js

---
+++ navbar.js
@@ -0,0 +1,16 @@
+export const navbar = () => `
+<ul>
+    <li>
+        <a href="./index.html">Homepage</a>
+    </li>
+    <li>
+        <a href="./recipe.html">Special Recipe</a>
+    </li>
+    <li>
+        <a href="./signup.html">Sign Up</a>
+    </li>
+    <li>
+        <a href="./login.html">Login</a>
+    </li>
+</ul>
+`
